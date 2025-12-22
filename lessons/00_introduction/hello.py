7c7740ce05d24b38b15151978f0b877ead4fb54f
Tsepo Mkwebu <tsepomkwebu@gmail.com>
2025-12-22 15:57:29 +0100

    Merge remote repository

--- lessons/00_introduction/hello.py
+++ lessons/00_introduction/hello.py
@@ -1,5 +1,14 @@
 def main():
-    print("Hello, world!")
+    x = int(input( "Co to jest x?"))
+    y = int(input ("Co to jest y?"))
+
+    if x < y:
+        print("x is less than y")
+    elif x > y:
+        print("x is greater than y")
+    elif x == y:
+        print("x is equals to y")
+    
 
 if __name__ == "__main__":
     main()
